cc657d65a42e1ad3ffff46cf946ae2f75c5f376b
rmkumar204 <rmkumar204@gmail.com>
2022-05-28 03:55:55 +0530

    another commit

--- src/Message.js
+++ src/Message.js
@@ -2,6 +2,7 @@ import React from 'react';
 function Message(props){
     const classPr=props.color?"color":"";
     //new commit
+    //another commit
     //here props.color is textcolor 
     return<h2 className={classPr}>Welcome</h2>
 }
